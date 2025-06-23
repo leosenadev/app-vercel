06ecb4008560f91d743ba0112f7b8bb3aebfb901
leosenadev <leo.sse@hotmail.com>
2025-06-23 15:29:51 -0300

    e

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
 
-    res.sendFile('index.html', { root: '/views' })
+    res.sendFile('./views/index.html')
 });
 
 app.get('/api/json', (req, res) => {

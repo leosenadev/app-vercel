faf3721ad99512f4ba718cb62b3f7e29feec3312
leosenadev <leo.sse@hotmail.com>
2025-06-24 21:57:02 -0300

    BACKEND 24-06-21:57

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server,{ cors: {
+    origin: "*"
+  }});
 app.use(logger('dev'));
 
 io.on('connection', (socket) => {
